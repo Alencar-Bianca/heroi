62c564e8ba9905fabeb2251e36c4c3053f8fb866
Alencar-Bianca <alencar-bianca@hotmail.com>
2021-12-30 22:39:26 -0300

    Select Hero

--- src/app/heroi/heroi.component.ts
+++ src/app/heroi/heroi.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { HEROIS } from './../mock-herois';
+import { Component } from '@angular/core';
 import { Hero } from './hero.model';
 
 @Component({
@@ -6,11 +7,13 @@ import { Hero } from './hero.model';
   templateUrl: './heroi.component.html',
   styleUrls: ['./heroi.component.css']
 })
-export class HeroiComponent implements OnInit {
+export class HeroiComponent  {
   hero: Hero =  {id: 1, name: 'Spider-Man'}
-  constructor() { }
 
-  ngOnInit(): void {
-  }
+herois = HEROIS;
+selectedHero?: Hero;
 
+  onSelect(hero : Hero): void{
+    this.selectedHero = hero;
+}
 }
---
+++ src/app/mock-herois.ts
@@ -0,0 +1,12 @@
+import { Hero } from './heroi/hero.model';
+
+export const HEROIS: Hero[] = [
+  {id:1,name:'Spider-Man'},
+  {id:2,name:'Wolverine'},
+  {id:3,name:'Mulher Maravilha'},
+  {id:4,name:'Batman'},
+  {id:5,name:'Homem de Ferro'},
+  {id:6,name:'Capitão América'},
+  {id:7,name:'Capitã Marvel'},
+  {id:8,name:'Mulher Gato'},
+]
