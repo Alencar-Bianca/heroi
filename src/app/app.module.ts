a582fb33ae41a4df6019e9b9ef16508e9f9a8202
Alencar-Bianca <alencar-bianca@hotmail.com>
2021-12-30 21:02:52 -0300

    Create the folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeroiComponent } from './heroi/heroi.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroiComponent
   ],
   imports: [
     BrowserModule,
